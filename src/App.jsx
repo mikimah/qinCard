d51897055c663eb46691268d917a50589eb9f378
mikimah <trinhnhatmiki@gmail.com>
2025-08-25 10:08:32 +0700

    seacond commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Home from './components/Home.jsx';
-
+import Home2 from './components/Home2.jsx';
 function App() {
 
 
   return (
     <>
-      <div className="border rounded-[2.25rem] border-dashed border-white bg-white/40 p-2 min-w-[53.6%] min-h-[69%] flex justify-center items-center">
-        <Home />
+      <div className="border rounded-[2.25rem] border-dashed border-white bg-white/40 p-6 m-4 flex justify-center items-center animate-slide-up min-md:p-9">
+        <Home2 />
       </div>
     </>
   )
---
+++ src/components/Home2.jsx
@@ -0,0 +1,76 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram,faFacebookMessenger,faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { useRef, useState } from 'react';
+
+function Home2() {
+  const words =["Hallo~ Qin at your service!","Ow! That wasn't nice of you...","Nyaw-!","UwU","I don't like that!","Oof!","I don't feel so good...","*explode.mp3*"];
+  const chatBubbleRef = useRef(null);
+  const pfpRef = useRef(null);
+  const explodeRef = useRef(null);
+  const [timeClicked, setTimeClicked] = useState(0);
+
+  function toggleAnimation() {
+    const chatBubble = chatBubbleRef.current;
+    if (!chatBubble ) return;
+    setTimeClicked(prev => prev+1);
+    if(timeClicked==6){
+      triggerExplosion();
+    }
+    else{
+    chatBubble.classList.toggle("animate-pop-out");
+    void chatBubble.offsetWidth; // ép reflow
+    chatBubble.classList.toggle("animate-pop-out");
+    }
+   
+    
+  }
+
+  function triggerExplosion() {
+    const pfp = pfpRef.current;
+    const explode = explodeRef.current;
+    if (!pfp || !explode) return;
+    explode.classList.toggle("hidden");
+    setTimeout(() => {
+      pfp.src="new-image.jpg"
+      explode.classList.toggle("hidden");
+    }, 2000);
+  }
+
+  return (
+    <div className='flex flex-col justify-center items-center gap-5'>
+      <div className="flex flex-col justify-center items-center gap-2 min-md:items-start">
+        <div className=' flex justify-center items-center flex-col'>
+        <div className='relative chat chat-start  w-[5rem] h-[5rem] min-md:w-[6.5rem] min-md:h-[6.5rem]'>
+          <img ref={pfpRef} onClick={timeClicked == 7 ? null : ()=>toggleAnimation()} className="hover:cursor-pointer w-[5rem] h-[5rem]  rounded-full box-border border border-white p-1 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] min-md:w-[6.5rem] min-md:h-[6.5rem]" src="pfp.jpg" alt="This is a pfp" />
+          <img src="explode.jpg" ref={explodeRef} alt="explosion image" className='hidden absolute w-[100%] h-[100%] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]' />
+          <div ref={chatBubbleRef} className='absolute left-[110%] animate-pop-out '>
+            <div data-theme="light" className='chat-bubble bg-white rounded-2xl pl-2 pr-4'>
+              <pre className='text-[.45rem] text-purple-500 font-Baloo2 font-bold min-md:text-[1rem] min-md:pr-2'>{words[timeClicked]}</pre>
+            </div>
+          </div>
+        </div>
+         
+        <h1 className="font-extrabold text-[1.75rem] tracking-[1px] min-md:text-[4rem] ">Qin</h1>
+        </div>
+        <p className="font-bold max-md:text-center text-[.80rem] tracking-[1px] min-md:text-[1.2rem]">I'm a freelancer artist will go brrrrmm just for you. Nice to meet you my future client ♡</p>
+      </div>
+      <div className='animate-slide-up w-[100%] bg-white/40 rounded-3xl border border-white flex flex-col items-center justify-center font-bold p-2 min-md:flex-row min-md:justify-between min-md:p-4 min-md:rounded-full'>
+        <button className='bg-white rounded-full w-[100%]  py-1.5 mb-2 text-[1.05rem] tracking-[1px] hover:scale-[1.02] hover:cursor-pointer transition-[1s] min-md:w-[62%] min-md:mb-0 min-md:py-2 min-md:text-[1.35rem]'>Commission</button>
+        <ul className=' flex justify-center items-center gap-2 min-md:w-[35%] min-md:justify-around '>
+            <a href=""><li className='bg-white rounded-full px-1.5 pt-1.5 text-[1.25rem] hover:scale-[1.2] hover:bg-purple-500/10 transition-[1s] min-md:text-[1.5rem] min-md:px-2 min-md:pt-2 min-md:pb-0.5'><FontAwesomeIcon icon={faInstagram} /></li></a>
+            <a href=""><li className='bg-white rounded-full px-1.5 pt-1.5 text-[1.25rem] hover:scale-[1.2] hover:bg-purple-500/10 transition-[1s] min-md:text-[1.5rem] min-md:px-2 min-md:pt-2 min-md:pb-0.5'><FontAwesomeIcon icon={faFacebookMessenger} /></li></a>
+            <a href=""><li className='bg-white rounded-full px-1.5 pt-1.5 text-[1.25rem] hover:scale-[1.2] hover:bg-purple-500/10 transition-[1s] min-md:text-[1.5rem] min-md:px-2 min-md:pt-2 min-md:pb-0.5'><FontAwesomeIcon icon={faXTwitter} /></li></a>
+            <a href=""><li className='bg-white rounded-full px-1.5 pt-1.5 text-[1.25rem] hover:scale-[1.2] hover:bg-purple-500/10 transition-[1s] min-md:text-[1.5rem] min-md:px-2 min-md:pt-2 min-md:pb-0.5'><FontAwesomeIcon icon={faEnvelope} /></li></a>
+        </ul>
+      </div>
+      <hr className='w-[100%] font-extrabold text-4xl'/>
+      <div className='h-auto  w-[100%] flex justify-center min-md:justify-start'>
+        <button className='p-1 px-1.5 rounded-2xl bg-gray-400 text-[.5rem] text-gray-200 duration-200 ease-in-out hover:scale-[1.08] hover:text-white hover:bg-linear-to-br from-blue-500 to-purple-500 min-md:text-[.75rem]'>Made with love :3</button>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Home2;
